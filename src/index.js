40e6d5be58e0bf83c6f74fe7e0baa72955d49bfc
bad4iz <bad4iz@gmail.com>
2017-05-08 21:53:37 +0400

    router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import {Router, Route} from 'react-router';
 import ReactDOM from 'react-dom';
 import {Button} from './components/button';
 import {List} from './components/list';
+
 
 class App extends React.Component {
     
@@ -11,7 +13,7 @@ class App extends React.Component {
         this.state = {
             text: '',
             timer: 0,
-            array:[
+            array: [
                 {id: 1, name: 'Вася'},
                 {id: 2, name: 'Дима'},
                 {id: 3, name: 'Федя'},
@@ -21,39 +23,42 @@ class App extends React.Component {
     }
     
     componentWillMount() {
-        setInterval(()=> {
+        setInterval(() => {
             this.setState({timer: this.state.timer < 100 ? this.state.timer + 1 : 0});
         }, 100);
     }
-  
+    
     handleChange(e) {
         const text = e.target.value;
-        this.setState({ text });
+        this.setState({text});
     }
     
     render() {
         return (
-       <div className="demo-card-event mdl-card mdl-shadow--2dp" style={{margin: '100px auto', padding: '20px'}}>
-           <p>{this.state.timer}</p>
-           <h1 style={{margin: ' auto'}}>Прювет !</h1>
-           
-           <List />
-           
-           <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-             <input  onChange={this.handleChange.bind(this)}  className="mdl-textfield__input" type="text" id="sample3"/>
-               <label className="mdl-textfield__label" htmlFor="sample3">Введите текст...</label>
-           </div>
-           <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-               <p>{this.state.text}</p>
-           </div>
-           <div className="mdl-card__actions mdl-card--border">
-               {this.state.timer < 50 ? <Button btnText="Нажми кнопку"/>: null}
-           </div>
-       </div>);
+            <div className="demo-card-event mdl-card mdl-shadow--2dp" style={{margin: '100px auto', padding: '20px'}}>
+                <p>{this.state.timer}</p>
+                <h1 style={{margin: ' auto'}}>Прювет !</h1>
+                
+                <List />
+                
+                <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                    <input onChange={this.handleChange.bind(this)} className="mdl-textfield__input" type="text"
+                           id="sample3"/>
+                    <label className="mdl-textfield__label" htmlFor="sample3">Введите текст...</label>
+                </div>
+                <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                    <p>{this.state.text}</p>
+                </div>
+                <div className="mdl-card__actions mdl-card--border">
+                    {this.state.timer < 50 ? <Button btnText="Нажми кнопку"/> : null}
+                </div>
+            </div>);
     }
 }
 
 ReactDOM.render(
-   <App/>,
-   document.getElementById('app')
+    <Router>
+        <Route path="/" component={ App } />
+    </Router>,
+    document.getElementById('app')
 );

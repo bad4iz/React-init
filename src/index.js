585db8de5d2e79eeed2be330de4b07f57dba5116
bad4iz <bad4iz@gmail.com>
2017-05-08 20:03:32 +0400

    inputs setState

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ class App extends React.Component {
         super(props);
         
         this.state = {
-            text: 'ytyf'
+            text: ''
         };
     }
   
@@ -22,7 +22,7 @@ class App extends React.Component {
        <div className="demo-card-event mdl-card mdl-shadow--2dp" style={{margin: '100px auto', padding: '20px'}}>
            <h1 style={{margin: ' auto'}}>Прювет !</h1>
            <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-             <input value={this.state.text} onChange={this.handleChange}  className="mdl-textfield__input" type="text" id="sample3"/>
+             <input  onChange={this.handleChange.bind(this)}  className="mdl-textfield__input" type="text" id="sample3"/>
                <label className="mdl-textfield__label" htmlFor="sample3">Введите текст...</label>
            </div>
            <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
